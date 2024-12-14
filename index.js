c4e95cdeadb7fd6c0851d6c43c8dd918653a355b
Eric-TN2201 <trongnghia.eric@gmail.com>
2024-12-14 23:20:28 +0700

    done permalist
    - CRUD done

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user : "postgres",
+  host : "localhost",
+  database : "permalist",
+  password : "123456",
+  port : 5432
+});
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -12,22 +22,56 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+// get list
+app.get("/", async (req, res) => {
+  const result = await db.query("select * from items order by id asc");
+  items = [];
+  
+  result.rows.forEach(item => {
+    items.push(item);
+  });
+  console.log("list", items);
+  
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+// handle add - add new item todo
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try {
+    await db.query("insert into items (title) values ($1)", [item]);    
+    res.redirect("/");
+  } catch (err) {
+    console.error('add', err);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+// handle edit - edit title
+app.post("/edit", async (req, res) => {
+  const title = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try {
+    await db.query("update items set title = $1 where id = $2", [title, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.error('update', err);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+// handle delete
+app.post("/delete", async (req, res) => {
+  // const ids = req.body
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("delete from items where id = $1", [id]);
+    res.redirect("/");
+  } catch (error) {
+    console.error('delete', error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
